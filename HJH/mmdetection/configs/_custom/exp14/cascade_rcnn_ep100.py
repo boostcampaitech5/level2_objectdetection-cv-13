cfe0e384f1529438d5a205ebb1f48f5f7fe240e2
jh58power <jh58power@naver.com>
2023-05-19 07:59:41 +0000

    commit 0519

---
+++ HJH/mmdetection/configs/_custom/exp14/cascade_rcnn_ep100.py
@@ -0,0 +1,5 @@
+_base_ = [
+    'cascade_rcnn_r50_fpn_swin_L.py',
+    'dataset.py',
+    'my_schedule.py', 'default_runtime.py'
+]
---
+++ HJH/mmdetection/configs/_custom/exp8/faster_rcnn.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../../_base_/models/faster_rcnn_r50_fpn.py',
+    'dataset.py',
+    'my_schedule.py', 'default_runtime.py'
+]
