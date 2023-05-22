f8902b36b1968144bb4c393db60e1fbb5512ed5f
RyeongTaeKim <rlafudxo1000@naver.com>
2023-05-22 09:41:03 +0900

    Project Upload

--- HJH/mmdetection/configs/_custom/exp14/cascade_rcnn_ep100.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    'cascade_rcnn_r50_fpn_swin_L.py',
-    'dataset.py',
-    'my_schedule.py', 'default_runtime.py'
-]
--- HJH/mmdetection/configs/_custom/exp8/faster_rcnn.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    '../../_base_/models/faster_rcnn_r50_fpn.py',
-    'dataset.py',
-    'my_schedule.py', 'default_runtime.py'
-]
